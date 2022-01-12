19af4488fd47b293beb727ca043cb2268bf28eae
Eric Galand <eric.galand@gmail.com>
2022-01-12 09:01:31 +0100

    commit

---
+++ exo.js
@@ -0,0 +1,7 @@
+function correction(){
+var d=document.getElementById("valeur");
+d.innerHTML = "$$x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.$$";
+MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+}
+
+//MathJax.Hub.Queue(["Typeset",MathJax.Hub,"valeur"]);
