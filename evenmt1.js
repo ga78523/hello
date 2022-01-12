6a004947b647e7b92f85eede33bf9a09ac73fc11
Eric Galand <eric.galand@gmail.com>
2022-01-12 14:19:41 +0100

    third

--- evenmt1.js
+++ evenmt1.js
@@ -1,8 +1,12 @@
-let compteur = 0;
+/*let compteur = 0;
 function suivant() {
     compteur = compteur + 1;
     let v = document.getElementById("valeur");
     v.innerHTML = compteur;
 }
+*/
 let b = document.getElementById("valeur");
-b.addEventListener("click", suivant);
+b.addEventListener('click', function(){          // On écoute l'événement click
+    alert("C'est cliqué !");               // On change le contenu de notre élément pour afficher "C'est cliqué !"
+}
+);
